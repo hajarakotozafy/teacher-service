5de1b32ccba7c957e63d15bcf55099931bea3704
Haja <hajarakotozafy45@gmail.com>
2024-05-25 12:56:18 +0300

    test

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "hajanyaina45",
-    DB: "teacher-service",
+    HOST: "pg-50bd1911-1fb1-4680-88bb-05745d943012-teacher597876040-choreo.e.aivencloud.com",
+    PORT: "16644",
+    USER: "avnadmin",
+    PASSWORD: "AVNS_sMOWWYgSAaw09e2MpQp",
+    DB: "defaultdb",
     dialect: "postgres",
     pool: {
         max: 5,
--- src/models/index.js
+++ src/models/index.js
@@ -1,21 +1,24 @@
 const dbConfig = require("../config/db.config");
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    dbConfig.DB,
-    dbConfig.USER,
-    dbConfig.PASSWORD,
-    {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect,
-        pool: {
-            max: dbConfig.pool.max,
-            min: dbConfig.pool.min,
-            acquire: dbConfig.pool.acquire,
-            idle: dbConfig.pool.idle
-        }
-    }
-);
+// const sequelize = new Sequelize(
+//     dbConfig.DB,
+//     dbConfig.USER,
+//     dbConfig.PASSWORD,
+//     {
+//         host: dbConfig.HOST,
+//         port: dbConfig.PORT,
+//         dialect: dbConfig.dialect,
+//         pool: {
+//             max: dbConfig.pool.max,
+//             min: dbConfig.pool.min,
+//             acquire: dbConfig.pool.acquire,
+//             idle: dbConfig.pool.idle
+//         }
+//     }
+// );
+
+const sequelize = new Sequelize('postgres://avnadmin:AVNS_sMOWWYgSAaw09e2MpQp@pg-50bd1911-1fb1-4680-88bb-05745d943012-teacher597876040-choreo.e.aivencloud.com:16644/defaultdb')
 
 const db = {};
 db.Sequelize = Sequelize;
