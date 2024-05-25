fff20487228f6a825fdd287fa6a215572ae38e72
Haja <hajarakotozafy45@gmail.com>
2024-05-25 17:18:03 +0300

    fix: config sequelize

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    HOST: "pg-50bd1911-1fb1-4680-88bb-05745d943012-teacher597876040-choreo.e.aivencloud.com",
-    PORT: "16644",
-    USER: "avnadmin",
-    PASSWORD: "AVNS_sMOWWYgSAaw09e2MpQp",
-    DB: "defaultdb",
+    HOST: "dpg-cp7j90e3e1ms73alkiig-a.oregon-postgres.render.com",
+    USER: "haja",
+    PASSWORD: "LhV24S0yRGxvyw9qERKSh8D16oNSl4o6",
+    DB: "teacherservice",
     dialect: "postgres",
     pool: {
         max: 5,
--- src/models/index.js
+++ src/models/index.js
@@ -1,24 +1,39 @@
 const dbConfig = require("../config/db.config");
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//     dbConfig.DB,
-//     dbConfig.USER,
-//     dbConfig.PASSWORD,
-//     {
-//         host: dbConfig.HOST,
-//         port: dbConfig.PORT,
-//         dialect: dbConfig.dialect,
-//         pool: {
-//             max: dbConfig.pool.max,
-//             min: dbConfig.pool.min,
-//             acquire: dbConfig.pool.acquire,
-//             idle: dbConfig.pool.idle
-//         }
-//     }
-// );
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
+        },
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+            keepAlive: true,
+        },
+        ssl: true
 
-const sequelize = new Sequelize('postgres://avnadmin:AVNS_sMOWWYgSAaw09e2MpQp@pg-50bd1911-1fb1-4680-88bb-05745d943012-teacher597876040-choreo.e.aivencloud.com:16644/defaultdb')
+    }
+);
+
+// const sequelize = new Sequelize('postgres://haja:LhV24S0yRGxvyw9qERKSh8D16oNSl4o6@dpg-cp7j90e3e1ms73alkiig-a/teacherservice')
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 const db = {};
 db.Sequelize = Sequelize;
